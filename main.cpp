bb5699566b2769d6af9235edb4ef04ef257e81af
Felipe Alencar <feelipealeencar@gmail.com>
2022-03-31 22:40:38 -0300

    Arquivo cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // variable
+    int veiculo = 1, placa;
+
+    // start
+    cout << "Digite 1 para carro ou 2 para caminhao: ";
+    cin >> veiculo;
+
+    if(veiculo == 1)
+    {
+        cout << "Tipo de veiculo escolhido: CARRO " << endl;
+        cout << endl;
+        cout << "Digite o final da placa: ";
+
+    }
+
+
+
+
+
+
+    return 0;
+}
